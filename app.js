fab7825d529c89c353a84e70a1344761e8ed00fc
sdefrancesco <skaterbastian@aol.com>
2025-03-06 11:52:45 -0500

    finish part 1.4

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let addNumsInArray = (arr) => {
     return sum
 }
 
-console.log(`Sum of numbers array`, addNumsInArray(numbers))
+console.log(`Sum of numbers array`, addNumsInArray(numbers), '\n')
 
 // to get the average a group of numbers you have to add them all up and then divide it by the amount of numbers, so we can use our function created previously
 let avgOfNumbers = (arr) => {
@@ -22,4 +22,47 @@ let avgOfNumbers = (arr) => {
     return avg
 }
 
-console.log(`Average of numbers array`, avgOfNumbers(numbers))
+console.log(`Average of numbers array`, avgOfNumbers(numbers), '\n')
+
+
+// Take an array of strings and return the longest string.
+let strings = [
+    "i like cake",
+    "cat",
+    "clifford the big red dog",
+    "dog",
+]
+
+let returnLongestString = (arr) => {
+    // start string counter at 0
+    let longestStr = 0
+    // get the string index so we can display the actual string to the console
+    let longestStrIndex;
+    arr.forEach((str, i)=> {
+        if(str.length > longestStr) {
+            longestStr = str.length
+            longestStrIndex = i
+        }
+    })
+    console.log(`the longest string is ${strings[longestStrIndex]} at index ${longestStrIndex}, with a length of ${longestStr}`)
+    return longestStr
+}
+
+console.log(`The longest string in the array is`,returnLongestString(strings), '\n')
+
+
+// Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+
+let compareStringToNumber = (arrOfStrings, number) => {
+    // new array to push a string from the strings array if greater than the argument number
+    let newArr = []
+    arrOfStrings.forEach((str) => {
+        if(str.length > number) {
+            newArr.push(str)
+        }
+    })
+    return newArr
+}
+
+console.log(compareStringToNumber(strings, 5))
+
